6ce7cf4f78eb080f304836df3b5830d6794ecd94
Chris <causton@pro.causton.test>
2016-03-12 17:37:45 -0600

    Initial dot files

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+TS=$(date +%Y%m%dT%H%M%S)
+
+main() {
+  test -n "$HOME" || die '$HOME is empty'
+  test -e "$HOME" || die '$HOME does not exist'
+
+  if [ -e ~/.vimrc ]; then
+    mv -v ~/.vimrc /tmp/vimrc.${TS}
+  fi
+
+  for dotfile in bashrc bash_aliases vim gitconfig; do
+    target=$SCRIPT_DIR/$dotfile
+    link_name=$HOME/.$dotfile
+
+    if [ ! -e $link_name ]; then
+      # file doesn't exist
+      ln -sv $target $link_name
+    elif [ -L $link_name ]; then
+      # check existing symlink
+      curr_target=$(readlink $link_name)
+
+      if [ "$curr_target" != "$target" ]; then
+          unlink $link_name
+          ln -sv $target $link_name
+      fi
+    else
+      mv -v $link_name /tmp/${dotfile}.${TS}
+      ln -sv $target $link_name
+    fi
+  done
+}
+
+die() {
+  echo "$1"
+  exit 1
+}
+
+
+main
